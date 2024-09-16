958bc1d0e0e85d12d6ab4317db21e52c1dcbb18b
yayner2002 <yayner2002@gmail.com>
2024-09-16 17:29:15 -0400

    login page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,67 @@
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      {" "}
+      <form onSubmit={handleSubmit}>
+        <h1>Login Page</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            marginBottom: "4px",
+            marginTop: "4px",
+          }}
+        >
+          <label htmlFor="email">Email:</label>
+          <input
+            onChange={handleChange}
+            value={formData.email}
+            style={{
+              padding: "8px",
+            }}
+            type="email"
+            placeholder="Enter Your Email..."
+            name="email"
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            marginBottom: "4px",
+            marginTop: "4px",
+          }}
+        >
+          <label htmlFor="password">Password:</label>
+          <input
+            value={formData.password}
+            onChange={handleChange}
+            style={{
+              padding: "8px",
+            }}
+            type="password"
+            placeholder="Enter Your Password..."
+            name="password"
+          />
+        </div>
 
-export default Login
+        <div>
+          <button
+            style={{
+              marginBottom: "4px",
+              marginTop: "10px",
+              border: "2px solid #ccc",
+              textAlign: "center",
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
